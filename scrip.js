0a720d37613869e9b244a4f8cf94d06bfdf1a2f1
iMac G1 <imacg1@iMac-de-Energy-18.local>
2022-05-06 12:41:05 +0000

    third commit

--- scrip.js
+++ scrip.js
@@ -1,18 +1,19 @@
 
  function myfunction(){
+  //  recupérer les données des inputs
    let userinput1 = document.querySelector("#userinput1");
     let userinput2 = document.querySelector("#userinput2");
     let userinput3 = document.querySelector("#userinput3");
     let userinput4 = document.querySelector("#userinput4");
     let userinput5 = document.querySelector("#userinput5");
 
-
+//  stockage des données récuperées
    let message1 = document.querySelector("#message1");
     let message2 = document.querySelector("#message2");
     let message3 = document.querySelector("#message3");
     let message4 = document.querySelector("#message4");
     let message5 = document.querySelector("#message5");
-
+//  afficher les données recupérées
     message1.innerHTML= userinput1.value;
     message2.innerHTML= userinput2.value;
     message3.innerHTML= userinput3.value;
